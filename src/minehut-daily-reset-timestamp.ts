5b63bca060273dd7e02aa678e24c0e7eba9c4e3d
tarna <46247728+tarna@users.noreply.github.com>
2025-01-11 11:16:09 -0500

    fix: issue with the day of the timestamp

--- src/minehut-daily-reset-timestamp.ts
+++ src/minehut-daily-reset-timestamp.ts
@@ -3,6 +3,9 @@ import { Clipboard } from '@raycast/api';
 export default async function Command() {
     const resetTimestamp = new Date();
     resetTimestamp.setUTCHours(9, 0, 0, 0);
+    if (resetTimestamp.getTime() < Date.now()) {
+        resetTimestamp.setDate(resetTimestamp.getDate() + 1);
+    }
     const copyText = `<t:${Math.floor(resetTimestamp.getTime() / 1000)}:R>`;
     await Clipboard.copy(copyText);
 }
